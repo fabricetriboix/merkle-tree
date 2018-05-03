cb9a723e4ec11095e806ed304e5cb7d4e37f40dd
Fabrice Triboix <ftriboix@incise.co>
2018-05-03 18:19:59 +0100

    Start writing constructor

--- include/merkle-tree/merkle-tree.hpp
+++ include/merkle-tree/merkle-tree.hpp
@@ -7,6 +7,10 @@ extern "C" {
 
 #include <vector>
 #include <list>
+#include <stdexcept>
+
+/** Size of a single datum, in bytes */
+#define MERKLE_TREE_ELEMENT_SIZE 32
 
 class MerkleTree
 {
@@ -17,6 +21,11 @@ public :
      */
     typedef std::vector<uint8_t> Buffer;
 
+    /** List of elements
+     *
+     */
+    typedef std::list<Buffer> Elements;
+
     /** Layer
      *
      * TODO
@@ -25,8 +34,13 @@ public :
 
     /** Constructor
      *
+     * \param elements      [in] Elements to add to the Merkle Tree
+     * \param preserveOrder [in] Whether to preserve the elements order
+     *
+     * \throw `std::runtime_error` if `elements` contains elements which are
+     *        not of the right size, \see MERKLE_TREE_ELEMENT_SIZE.
      */
-    MerkleTree(const std::list<Buffer>& elements, bool preserveOrder = false);
+    MerkleTree(const Elements& elements, bool preserveOrder = false);
 
     /** Destructor */
     virtual ~MerkleTree();
@@ -37,8 +51,16 @@ public :
      */
     //const Layer& getRoot() const;
 
+    // XXX getProof(XXX element, XXX hex) const;
+
+    // XXX getProofOrdered(XXX element, XXX hex) const;
+
 private :
-    int dummy_;
+    Elements elements_;
 };
+
+// XXX bool checkProofOrdered(proof, root, element, index);
+// XXX bool checkProof(proof, root, element);
+// XXX merkleRoot(elements, preserveOrder);
 
 #endif // MERKLE_TREE_HPP_
--- src/merkle-tree/merkle-tree.cpp
+++ src/merkle-tree/merkle-tree.cpp
@@ -1,8 +1,28 @@
 #include "merkle-tree/merkle-tree.hpp"
+#include <sstream>
 
 MerkleTree::MerkleTree(const std::list<Buffer>& elements, bool preserveOrder)
-    : dummy_(0)
 {
+    for (   Elements::const_iterator it = elements.begin();
+            it != elements.end();
+            ++it) {
+        if (it->empty()) {
+            continue; // ignore empty elements
+        }
+        if (it->size() != MERKLE_TREE_ELEMENT_SIZE) {
+            std::ostringstream oss;
+            oss << "Element size is " << it->size() << ", it must be "
+                << MERKLE_TREE_ELEMENT_SIZE;
+            throw std::runtime_error(oss.str());
+        }
+    } // for each element
+
+    if (!preserveOrder) {
+        elements_.unique(); // remove duplicates
+        elements_.sort(); // sort elements
+    }
+
+    // layers_ = XXX;
 }
 
 MerkleTree::~MerkleTree()
