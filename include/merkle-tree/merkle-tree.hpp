d2ecdc303ff5d88655be56dab146b70e1384ec95
Fabrice Triboix <ftriboix@incise.co>
2018-05-04 09:05:02 +0100

    Store elements in a deque instead of a list
    This is to allow better random access, which will be needed later.
    
    Also, starting to add code to build the layers.

--- include/merkle-tree/merkle-tree.hpp
+++ include/merkle-tree/merkle-tree.hpp
@@ -6,39 +6,43 @@ extern "C" {
 }
 
 #include <vector>
-#include <list>
+#include <deque>
 #include <stdexcept>
 
-/** Size of a single datum, in bytes */
-#define MERKLE_TREE_ELEMENT_SIZE 32
+/** Size of a hash, in bytes
+ *
+ * We are using Blake2b with an output of 128 bits, which is 16 bytes.
+ */
+#define MERKLE_TREE_ELEMENT_SIZE 16
 
 class MerkleTree
 {
 public :
     /** Buffer type
      *
-     * This represents a single datum in the Merkle Tree.
+     * This represents a single hash in the Merkle Tree and must have a
+     * length of `MERKLE_TREE_ELEMENT_SIZE`.
+     *
+     * \see MERKLE_TREE_ELEMENT_SIZE
      */
     typedef std::vector<uint8_t> Buffer;
 
     /** List of elements
      *
+     * This is a list of hashes.
      */
-    typedef std::list<Buffer> Elements;
-
-    /** Layer
-     *
-     * TODO
-     */
-    //typedef TODO Layer;
+    typedef std::deque<Buffer> Elements;
 
     /** Constructor
      *
      * \param elements      [in] Elements to add to the Merkle Tree
+     *                           There must be at least one element
      * \param preserveOrder [in] Whether to preserve the elements order
      *
      * \throw `std::runtime_error` if `elements` contains elements which are
      *        not of the right size, \see MERKLE_TREE_ELEMENT_SIZE.
+     *
+     * \throw `std::runtime_error` if `elements` is empty
      */
     MerkleTree(const Elements& elements, bool preserveOrder = false);
 
@@ -56,7 +60,22 @@ public :
     // XXX getProofOrdered(XXX element, XXX hex) const;
 
 private :
-    Elements elements_;
+    /** Layers data structure
+     *
+     * This data structure represents the various layers of the Merkle Tree.
+     * The first layer is the initial list of hashes, the 2nd layer is the
+     * combination of the hashes of the first layer, etc. until the last layer
+     * which is the top-level hash, aka the root. The last layer has a length
+     * of one.
+     */
+    typedef std::deque<Elements> Layers;
+
+    bool     preserveOrder_; /**< Whether to preserver the initial order */
+    Elements elements_;      /**< Leaves of the Merkle Tree */
+    Layers   layers_;        /**< The actual Merkle Tree */
+
+    /** Build the Merkle Tree layers */
+    void getLayers();
 };
 
 // XXX bool checkProofOrdered(proof, root, element, index);
--- src/merkle-tree/merkle-tree.cpp
+++ src/merkle-tree/merkle-tree.cpp
@@ -1,8 +1,13 @@
 #include "merkle-tree/merkle-tree.hpp"
 #include <sstream>
+#include <algorithm>
 
-MerkleTree::MerkleTree(const std::list<Buffer>& elements, bool preserveOrder)
+MerkleTree::MerkleTree(const Elements& elements, bool preserveOrder)
 {
+    if (elements.empty()) {
+        throw std::runtime_error("Empty elements list");
+    }
+
     for (   Elements::const_iterator it = elements.begin();
             it != elements.end();
             ++it) {
@@ -15,16 +20,47 @@ MerkleTree::MerkleTree(const std::list<Buffer>& elements, bool preserveOrder)
                 << MERKLE_TREE_ELEMENT_SIZE;
             throw std::runtime_error(oss.str());
         }
+        if (!preserveOrder) {
+            // Check that this element has not been pushed yet
+            if (std::find(elements_.begin(), elements_.end(), *it)
+                    != elements_.end()) {
+                continue; // ignore duplicates
+            }
+        }
+        elements_.push_back(*it);
     } // for each element
 
     if (!preserveOrder) {
-        elements_.unique(); // remove duplicates
-        elements_.sort(); // sort elements
+        std::sort(elements_.begin(), elements_.end()); // sort elements
     }
 
-    // layers_ = XXX;
+    getLayers();
 }
 
 MerkleTree::~MerkleTree()
 {
 }
+
+void MerkleTree::getLayers()
+{
+    layers_.clear();
+
+    // The first layer is the elements themselves
+    layers_.push_back(elements_);
+
+    if (elements_.empty()) {
+        return; // nothing left to do
+    }
+
+    // For subsequent layers, combine each pair of hashes in the previous
+    // layer to build the current layer. Repeat until the current layer has
+    // only one hash (this will be the root of the tree).
+//    while (layers_.back().size() > 1) {
+//        getNextLayer();
+//    }
+}
+
+//void MerkleTree::getNextLayer()
+//{
+//    const Elements& current_layer = layers_.back();
+//}
